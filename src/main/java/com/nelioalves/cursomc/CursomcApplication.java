1bbe4f3b3ad51162f48a2ec55dae4e93f95ea9e4
Heder silva <heder.e.silva@gmail.com>
2020-12-08 16:17:07 -0300

    Configuração e envio do projeto para produção

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
-
+	//mysql://b91c9ae9c9977f:9d2a7098@us-cdbr-east-02.cleardb.com/heroku_51fb91e284838a8?reconnect=true
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
