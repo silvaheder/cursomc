fdc11ece4646a3fd0f4d58c857fe035346cb8427
Heder silva <heder.e.silva@gmail.com>
2020-12-11 18:16:28 -0300

    Esqueci a senha

--- src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nelioalves/cursomc/config/SecurityConfig.java
@@ -48,8 +48,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			"/categorias/**",
 	};
 	
-	private static final String[] PUBLIC_MATCHERS_POST= {			
+	private static final String[] PUBLIC_MATCHERS_POST= {
+			
 			"/clientes/**",
+			"/auth/forgot/**"
 	};
 	
 	protected void configure(HttpSecurity http) throws Exception {
--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -2,10 +2,14 @@ package com.nelioalves.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
+	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
