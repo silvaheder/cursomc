b426f0fd422c7a5b44a4a8e5e9df847d13429833
Heder silva <heder.e.silva@gmail.com>
2020-12-11 11:10:59 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/nelioalves/cursomc/resources/ClienteResources.java
+++ src/main/java/com/nelioalves/cursomc/resources/ClienteResources.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -58,6 +59,7 @@ public class ClienteResources {
 		
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Integer id){
 		
@@ -65,6 +67,7 @@ public class ClienteResources {
 		return ResponseEntity.noContent().build();
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<ClienteDTO>> findAll() throws ObjectNotFoundException {
 		
@@ -73,6 +76,7 @@ public class ClienteResources {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/page" , method = RequestMethod.GET)
 	public ResponseEntity<Page<ClienteDTO>> findPage(
 			
