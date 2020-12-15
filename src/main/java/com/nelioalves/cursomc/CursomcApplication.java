9f2b6152bb3af627fdc9e8fbf9ad6877132afc14
Heder silva <heder.e.silva@gmail.com>
2020-12-15 18:39:19 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/nelioalves/cursomc/CursomcApplication.java
+++ src/main/java/com/nelioalves/cursomc/CursomcApplication.java
@@ -1,13 +1,19 @@
 package com.nelioalves.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.nelioalves.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 	
 	//mysql://b91c9ae9c9977f:9d2a7098@us-cdbr-east-02.cleardb.com/heroku_51fb91e284838a8?reconnect=true
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -15,7 +21,8 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-				
+		
+		s3Service.uploadFile("/home/heder/Downloads/img1.jpeg");
 	}
 
 }
